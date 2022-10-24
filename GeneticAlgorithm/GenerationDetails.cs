be1cdf976b4e40a5190a85f4fb52b1a9bdeda952
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-10-24 12:57:51 -0400

    start IGeneration implementation

--- GeneticAlgorithm/GenerationDetails.cs
+++ GeneticAlgorithm/GenerationDetails.cs
@@ -4,22 +4,37 @@ namespace GeneticAlgorithm
     {
 
 
-        private Chromosome[] _chromosomeArr;
+        private IChromosome[] _chromosomeArr; //change later to chromosome??
+        private IGeneticAlgorithm _geneticAlgorithm;
+
+        private double AverageFitness => throw new System.NotImplementedException();
+
+        private double MaxFitness => throw new System.NotImplementedException();
+
+        private long NumberOfChromosomes => throw new System.NotImplementedException();
 
         // regular constructor
         public GenerationDetails(IGeneticAlgorithm geneticAlgorithm ,FitnessEventHandler fitnessHandler, int seed) {
-
+            _geneticAlgorithm = geneticAlgorithm;
         }
 
         // copy constructor
         public GenerationDetails() {
+            
+        }
+
+        public IChromosome this[int index] {
 
         }
 
-        int PopulationSize { get; }
-        int NumberOfGenes { get; }
-        int LengthOfGene { get; }
-        double MutationRate { get; }
-        double EliteRate { get; }
+        public IChromosome SelectParent() {
+            
+        }
+
+        public void EvaluateFitnessOfPopulation() {
+
+        }
+
+
     }
 }
