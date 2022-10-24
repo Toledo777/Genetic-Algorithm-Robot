455dce2a398be1c0089400b251ab812cc909b76c
Davit_V <davit.voskerchyan@dawsoncollege.qc.ca>
2022-10-24 12:53:06 -0400

    Add population Size input

--- RobbyIterationGenerator/Program.cs
+++ RobbyIterationGenerator/Program.cs
@@ -11,21 +11,26 @@ namespace RobbyIterationGenerator
             // string top_level_folder = "/Generated-Files/";        
             // Console.WriteLine(Environment.CurrentDirectory);
             // User input for path
-            Console.Write("In what folder do you want to save the text files: ");
-            var folder_name = Console.ReadLine();
-            // Combine User Path to local direcotry
-            var path = System.IO.Path.Combine(Environment.CurrentDirectory, folder_name);
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-                Console.WriteLine($"\n\n Your generated text files will be saved in \n {path}");
-            }
-            else
-            {
-                Console.WriteLine($"{folder_name} folder already exists in {path} \n Files will be created in it.");
-            }
+            // Console.Write("In what folder do you want to save the text files: ");
+            // var folder_name = Console.ReadLine();
+            // // Combine User Path to local direcotry
+            // var path = System.IO.Path.Combine(Environment.CurrentDirectory, folder_name);
+            // if (!Directory.Exists(path))
+            // {
+            //     Directory.CreateDirectory(path);
+            //     Console.WriteLine($"\n\n Your generated text files will be saved in \n {path}");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"{folder_name} folder already exists in {path} \n Files will be created in it.");
+            // }
 
-            // 
+            // Input parameters for the generations
+            Console.WriteLine("How many is the population Size? : ");
+            int populationSize = int.Parse(Console.ReadLine());
+
+
+
 
         }
     }
