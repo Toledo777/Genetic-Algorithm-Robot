0d0ccbb6877ec1b78b0c6b7f4e967fffeabac9c5
Maxime_Rayvich <maximerayvich@gmail.com>
2022-10-24 11:33:42 -0400

    branch init

---
+++ GeneticAlgorithm/Chromosome.cs
@@ -0,0 +1,16 @@
+namespace GeneticAlgorithm
+{
+    public class Chromosome : IChromosome
+    {
+        
+        public Chromosome(int numGenes, int geneLength, int seed=0) //check what default seed should look like
+        {
+
+        }
+        public Chromosome(Chromosome chromosonme)
+        {
+
+        }
+
+    }
+}
