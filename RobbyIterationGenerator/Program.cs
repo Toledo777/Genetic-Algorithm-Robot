885bd7b34572a5af6ba4cdc0f3588145dc2db6f4
Davit_V <davit.voskerchyan@dawsoncollege.qc.ca>
2022-10-24 12:09:27 -0400

    Made user input path creation in local Enviroment

--- RobbyIterationGenerator/Program.cs
+++ RobbyIterationGenerator/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
 
 namespace RobbyIterationGenerator
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // string top_level_folder = "/Generated-Files/";        
+            // Console.WriteLine(Environment.CurrentDirectory);
+            // User input for path
+            Console.Write("In what folder do you want to save the text files: ");
+            var folder_name = Console.ReadLine();
+            // Combine User Path to local direcotry
+            var path = System.IO.Path.Combine(Environment.CurrentDirectory, folder_name);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                Console.WriteLine($"\n\n Your generated text files will be saved in \n {path}");
+            }
+            else
+            {
+                Console.WriteLine($"{folder_name} folder already exists in {path} \n Files will be created in it.");
+            }
+
+            // 
+
         }
     }
 }
