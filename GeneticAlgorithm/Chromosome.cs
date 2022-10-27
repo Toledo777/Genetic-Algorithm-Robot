7f24e11b5616953489fd4c5379cd72b94ceaa064
Maxime_Rayvich <maximerayvich@gmail.com>
2022-10-27 11:24:47 -0400

    chromosomes copy constructor and reproduce

--- GeneticAlgorithm/Chromosome.cs
+++ GeneticAlgorithm/Chromosome.cs
@@ -3,20 +3,18 @@ using System;
 
 namespace GeneticAlgorithm
 {
-    public class Chromosome : IChromosome
+    internal class Chromosome : IChromosome
     {
-        private int _numGenes;
         private Random _rng;
-        private int[] _genes;
-        private double _fitness;
         private int? _seed = null;
 
         public double Fitness {
-            get {return this._fitness;}
+            get;
+            set;
         }
 
         public int[] Genes {
-            get{return _genes;}
+            get;
         }
 
         public long Length{
@@ -24,29 +22,30 @@ namespace GeneticAlgorithm
         }
 
         public int this[int index] {
-
-        get{return this._genes[index];}
-        }
-
-        //Check if we should take fitness as param or not and create overloaded constructor for it which takes a seed.
+            get{return this.Genes[index];}
+            set {this.Genes[index] = value;}
+            }
+        public int NumGenes => Genes.Length;
+        public int? Seed => _seed;
+        public Random RNG => this._rng;
         public Chromosome(int numGenes, long geneLength, int? seed = null)
         {
             this._seed = seed;
-            this._numGenes = numGenes;
             Length = geneLength;
-            this._genes = new int[geneLength];
+            Genes= new int[geneLength];
+            Fitness = 0;
             if(seed != null){
                 this._rng = new Random((int)seed);
             }
             else{
                 this._rng = new Random();
             }
-            for(int i = 0; i < this._genes.Length; i++){{
-                _genes[i] = _rng.Next(1,8); // random number between 1 and 7.
+            for(int i = 0; i < this.Genes.Length; i++){{
+                Genes[i] = RNG.Next(1,8); // random number between 1 and 7.
             }}
 
         }
-        //when creating a child, the genes are already defined.
+        //when creating a child, the genes are already defined. Thus this overloaded constructor allows for the creation of children with predefined genes.
         public Chromosome(int numGenes, long geneLength, int[] genes, int? seed = null){
             this._seed = seed;
              if(seed != null){
@@ -55,36 +54,51 @@ namespace GeneticAlgorithm
             else{
                 this._rng = new Random();
             }
-            this._numGenes = numGenes;
             Length = geneLength;
-            this._genes = genes;
+            Fitness = 0;
+            this.Genes= genes;
         }
 
 
 
-        public Chromosome(Chromosome chromosome)
-        {
-            // this._rng = new Random(chromosome._rng);
-
+        public Chromosome(Chromosome chromosome): this(chromosome.NumGenes, chromosome.Length, chromosome.Genes, chromosome.Seed){
         }
 
         public IChromosome[] Reproduce(IChromosome spouse, double mutationProb)
         {
-            IChromosome[] children;
-            //first, find the range of genes to swap.
+            IChromosome[] children = new Chromosome[2];
+            children[0] = MutateChild(GenerateChild(this, (Chromosome)spouse), mutationProb);
+            children[1] = MutateChild(GenerateChild((Chromosome) spouse, this), mutationProb);
+            return children;
         }
-        private IChromosome generateChild(IChromosome parent1, IChromosome parent2, int startIndP2, int endIndP2){
+        
+        //Takes 2 chromosomes and modifies the first one with genes from the sceond one with a range chosen randomly.
+        private Chromosome GenerateChild(Chromosome parent1, Chromosome parent2){
+            int startIndP2 = RNG.Next(0, parent2.NumGenes-1); // I think the -1 should make sure that we always get at least 1 place to mutate
+            int endIndP2 = RNG.Next(startIndP2, this.NumGenes);
             //genes to swap
             int[] genes = parent1.Genes;
             for(int i = startIndP2; i < endIndP2; i++){
                 genes[i] = parent2[i];
             }
-            Chromosome child = new Chromosome();
+            Chromosome child = new Chromosome(NumGenes, parent1.Length, genes, Seed); //need to check that NumGenes and Seed get the right things.
+            return child;
+        }
+
+        private IChromosome MutateChild(Chromosome child, double mutationProb){
+            for(int i = 0; i < child.NumGenes; i++){
+                if(RNG.NextDouble() <= mutationProb){
+                    child[i] = RNG.Next(1,8);
+                }
+            }
+            return child;
         }
 
         public int CompareTo([AllowNull] IChromosome other)
         {
-            throw new System.NotImplementedException();
+            if(this.Fitness > other.Fitness){
+
+            }
         }
     }
 }
