e89d8dd7f4f6364c050f3305676f71b01a5a90a2
Maxime_Rayvich <maximerayvich@gmail.com>
2022-10-28 16:35:42 -0400

    comment on chromosomes

--- GeneticAlgorithm/Chromosome.cs
+++ GeneticAlgorithm/Chromosome.cs
@@ -28,6 +28,7 @@ namespace GeneticAlgorithm
         public int NumGenes => Genes.Length;
         public int? Seed => _seed;
         public Random RNG => this._rng;
+        //constructor 1
         public Chromosome(int numGenes, long geneLength, int? seed = null)
         {
             this._seed = seed;
@@ -45,7 +46,8 @@ namespace GeneticAlgorithm
             }}
 
         }
-        //when creating a child, the genes are already defined. Thus this overloaded constructor allows for the creation of children with predefined genes.
+        //constructor 2:
+        // when creating a child, the genes are already defined. Thus this overloaded constructor allows for the creation of children with predefined genes.
         public Chromosome(int numGenes, long geneLength, int[] genes, int? seed = null){
             this._seed = seed;
              if(seed != null){
@@ -60,10 +62,11 @@ namespace GeneticAlgorithm
         }
 
 
-
+        //constructor 3
         public Chromosome(Chromosome chromosome): this(chromosome.NumGenes, chromosome.Length, chromosome.Genes, chromosome.Seed){
         }
-
+        //takes another chromosome and creates and returns an array of two children chromosomes with it.
+        //mutation prob: chance of any gene to mutate into a random gene (0.1 = 10%)
         public IChromosome[] Reproduce(IChromosome spouse, double mutationProb)
         {
             IChromosome[] children = new Chromosome[2];
@@ -74,6 +77,7 @@ namespace GeneticAlgorithm
         
         //Takes 2 chromosomes and modifies the first one with genes from the sceond one with a range chosen randomly.
         private Chromosome GenerateChild(Chromosome parent1, Chromosome parent2){
+            //we want to create a start and end index, these positions will be the ones whose genes are swapped with the second parent.
             int startIndP2 = RNG.Next(0, parent2.NumGenes-1); // I think the -1 should make sure that we always get at least 1 place to mutate
             int endIndP2 = RNG.Next(startIndP2, this.NumGenes);
             //genes to swap
@@ -84,7 +88,7 @@ namespace GeneticAlgorithm
             Chromosome child = new Chromosome(NumGenes, parent1.Length, genes, Seed); //need to check that NumGenes and Seed get the right things.
             return child;
         }
-
+        //takes a chromosome and mutates it at a rate of the mutationProb (0.1 = 10%)
         private IChromosome MutateChild(Chromosome child, double mutationProb){
             for(int i = 0; i < child.NumGenes; i++){
                 if(RNG.NextDouble() <= mutationProb){
@@ -94,11 +98,17 @@ namespace GeneticAlgorithm
             return child;
         }
 
-        public int CompareTo([AllowNull] IChromosome other)
+        public int CompareTo(IChromosome other)
         {
-            if(this.Fitness > other.Fitness){
-
+            if(this.Fitness == other.Fitness){
+                return 0;
             }
+            else if(this.Fitness > other.Fitness){
+                return 1;
+            }
+            else{
+                return -1;
+            }    
         }
     }
 }
