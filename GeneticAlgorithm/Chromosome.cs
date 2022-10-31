4db4e351441c58163883b59de67f4cc43cb31e91
Maxime_Rayvich <maximerayvich@gmail.com>
2022-10-31 12:58:40 -0400

    add some better documentation

--- GeneticAlgorithm/Chromosome.cs
+++ GeneticAlgorithm/Chromosome.cs
@@ -10,7 +10,7 @@ namespace GeneticAlgorithm
 
         public double Fitness {
             get;
-            set;
+            internal set;
         }
 
         public int[] Genes {
@@ -27,13 +27,13 @@ namespace GeneticAlgorithm
             }
         public int NumGenes => Genes.Length;
         public int? Seed => _seed;
-        public Random RNG => this._rng;
+       
         //constructor 1
         public Chromosome(int numGenes, long geneLength, int? seed = null)
         {
             this._seed = seed;
             Length = geneLength;
-            Genes= new int[geneLength];
+            Genes= new int[numGenes];
             Fitness = 0;
             if(seed != null){
                 this._rng = new Random((int)seed);
@@ -42,13 +42,14 @@ namespace GeneticAlgorithm
                 this._rng = new Random();
             }
             for(int i = 0; i < this.Genes.Length; i++){{
-                Genes[i] = RNG.Next(1,8); // random number between 1 and 7.
+                Genes[i] = _rng.Next(1,8); // random number between 1 and 7.
             }}
 
         }
         //constructor 2:
         // when creating a child, the genes are already defined. Thus this overloaded constructor allows for the creation of children with predefined genes.
-        public Chromosome(int numGenes, long geneLength, int[] genes, int? seed = null){
+        public Chromosome(long geneLength, int[] genes, int? seed = null)
+        {
             this._seed = seed;
              if(seed != null){
                 this._rng = new Random((int)seed);
@@ -63,7 +64,7 @@ namespace GeneticAlgorithm
 
 
         //constructor 3
-        public Chromosome(Chromosome chromosome): this(chromosome.NumGenes, chromosome.Length, chromosome.Genes, chromosome.Seed){
+        public Chromosome(Chromosome chromosome): this(chromosome.Length, chromosome.Genes, chromosome.Seed){
         }
         //takes another chromosome and creates and returns an array of two children chromosomes with it.
         //mutation prob: chance of any gene to mutate into a random gene (0.1 = 10%)
@@ -78,26 +79,31 @@ namespace GeneticAlgorithm
         //Takes 2 chromosomes and modifies the first one with genes from the sceond one with a range chosen randomly.
         private Chromosome GenerateChild(Chromosome parent1, Chromosome parent2){
             //we want to create a start and end index, these positions will be the ones whose genes are swapped with the second parent.
-            int startIndP2 = RNG.Next(0, parent2.NumGenes-1); // I think the -1 should make sure that we always get at least 1 place to mutate
-            int endIndP2 = RNG.Next(startIndP2, this.NumGenes);
+            int startIndP2 = _rng.Next(0, parent2.NumGenes-1); // I think the -1 should make sure that we always get at least 1 place to mutate
+            int endIndP2 = _rng.Next(startIndP2, this.NumGenes);
             //genes to swap
             int[] genes = parent1.Genes;
             for(int i = startIndP2; i < endIndP2; i++){
                 genes[i] = parent2[i];
             }
-            Chromosome child = new Chromosome(NumGenes, parent1.Length, genes, Seed); //need to check that NumGenes and Seed get the right things.
+            Chromosome child = new Chromosome(parent1.Length, genes, Seed); //need to check that NumGenes and Seed get the right things.
             return child;
         }
         //takes a chromosome and mutates it at a rate of the mutationProb (0.1 = 10%)
         private IChromosome MutateChild(Chromosome child, double mutationProb){
             for(int i = 0; i < child.NumGenes; i++){
-                if(RNG.NextDouble() <= mutationProb){
-                    child[i] = RNG.Next(1,8);
+                if(_rng.NextDouble() <= mutationProb){
+                    child[i] = _rng.Next(1,8);
                 }
             }
             return child;
         }
-
+ 
+        /// <summary>
+        /// Compares two IChromosomes based on their fitness.
+        /// </summary>
+        /// <param name="other">The Chromosome to compare the current one with</param>
+        /// <returns> 0 if the IChromosomes' fitness are equal, 1 if the current one's is higher, -1 if it is lower. </returns>
         public int CompareTo(IChromosome other)
         {
             if(this.Fitness == other.Fitness){
