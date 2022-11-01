fa251a229ae5886431b4d782631dcb2ce56115a7
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-11-01 15:14:00 -0400

    refactor EvaluateFitnessOfPopulation to call handler depending on NumberOfTrials

--- GeneticAlgorithm/Generation.cs
+++ GeneticAlgorithm/Generation.cs
@@ -14,7 +14,8 @@ namespace GeneticAlgorithm
         private const int _parentRange = 10;
 
         // constructor, only called the first time
-        public Generation(IGeneticAlgorithm geneticAlgorithm, FitnessEventHandler fitnessHandler, int? seed = null) {
+        public Generation(IGeneticAlgorithm geneticAlgorithm, FitnessEventHandler fitnessHandler, int? seed = null) 
+        {
             _seed = seed;
             _geneticAlgorithm = geneticAlgorithm;
             _fitnessHandler  = fitnessHandler;
@@ -23,7 +24,8 @@ namespace GeneticAlgorithm
         }
 
         // copy constructor
-        public Generation(IChromosome[] chromArr) {
+        public Generation(IChromosome[] chromArr) 
+        {
             _chromosomeArr = new IChromosome[chromArr.Length];
 
             for (int i = 0; i < chromArr.Length; i++) {
@@ -36,27 +38,32 @@ namespace GeneticAlgorithm
         }
 
         // property
-        public long NumberOfChromosomes {
+        public long NumberOfChromosomes 
+        {
             get {return _chromosomeArr.Length; }
         }
 
         // property
-        public double AverageFitness {
+        public double AverageFitness 
+        {
             get;
             set;
         }
         
         // property
-        public double MaxFitness {
+        public double MaxFitness 
+        {
             get;
             set;
         }
 
         // helper function
-        private double calculateAverageFitness() {
+        private double calculateAverageFitness() 
+        {
             // sum of fitness of all chromosomes
             double sum = 0;
-            for (int i = 0; i < _chromosomeArr.Length; i++) {
+            for (int i = 0; i < _chromosomeArr.Length; i++) 
+            {
                 sum+= _chromosomeArr[i].Fitness;
             }
 
@@ -65,11 +72,13 @@ namespace GeneticAlgorithm
         }
 
         // helper function
-        private double calculateMaxFitness() {
+        private double calculateMaxFitness() 
+        {
             double max = 0;
 
             // loop chromosomes to get max
-            for (int i = 0; i < _chromosomeArr.Length; i++) {
+            for (int i = 0; i < _chromosomeArr.Length; i++) 
+            {
                 if (_chromosomeArr[i].Fitness > max) {
                     max = _chromosomeArr[i].Fitness;
                 }
@@ -78,13 +87,15 @@ namespace GeneticAlgorithm
         }
 
         // Retrieves the IChromosome from the generation
-        public IChromosome this[int index] {
+        public IChromosome this[int index] 
+        {
             get { return _chromosomeArr[index]; }
             set { _chromosomeArr[index] = value; }
         }
 
         // select random parents from top range
-        public IChromosome SelectParent() {
+        public IChromosome SelectParent() 
+        {
             // sort array using CompareTo method of chromosome
             Array.Sort(_chromosomeArr);
             Random rng;
@@ -97,11 +108,25 @@ namespace GeneticAlgorithm
             return _chromosomeArr[randParentIndex];
         }
 
-        // unsure about how this function should be made
         public void EvaluateFitnessOfPopulation() {
-            for (int i = 0; i < _chromosomeArr.Length; i++) {
-                // call fitness handler for each chromosome
-               _chromosomeArr[i].Fitness = _fitnessHandler(_chromosomeArr[i], this);
+            // evaluate fitness of each chromosome in array
+            for (int i = 0; i < _geneticAlgorithm.PopulationSize; i++) 
+            {
+                if (_geneticAlgorithm.NumberOfTrials > 0)
+                {
+                    double fitnessSum = 0;
+                    for (int j = 0; j < _geneticAlgorithm.NumberOfTrials; j++) 
+                    {
+                        fitnessSum += _fitnessHandler(_chromosomeArr[i], this);
+                    }
+                    // assign fitness to be average of all trials for that chromosome
+                    _chromosomeArr[i].Fitness = fitnessSum / _geneticAlgorithm.NumberOfTrials;
+                }
+                
+                // when number of trials is 0 or negative
+                else {
+                    throw new InvalidOperationException("Cannot compute fitness without at least 1 trial");
+                }     
             }
         }
 
