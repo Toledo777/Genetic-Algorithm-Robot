1a4cf8711f4c2d28a29040a7d83726d16fff4c26
Davit_V <davit.voskerchyan@dawsoncollege.qc.ca>
2022-11-03 10:48:52 -0400

    Refactor GenreateGeneration Implementation

--- GeneticAlgorithm/GeneticAlgorithm.cs
+++ GeneticAlgorithm/GeneticAlgorithm.cs
@@ -38,7 +38,7 @@ namespace GeneticAlgorithm
             this.NumberOfTrials = numberOfTrials;
             this.FitnessCalculation = fitnessCalculation;
             this._seed = seed;
-            this._eliteChromosomePopulationSize = (int)(this.PopulationSize * (this.EliteRate/100));
+            this._eliteChromosomePopulationSize = (int)(this.PopulationSize * (this.EliteRate / 100));
             if (seed != null)
             {
                 _random = new Random((int)seed);
@@ -77,95 +77,58 @@ namespace GeneticAlgorithm
                 {
                     generation[i] = new Chromosome(this.NumberOfGenes, this.LengthOfGene, this._seed);
                 }
+                
                 this.GenerationCount++;
-
                 this.CurrentGeneration = generation;
+
                 return generation;
 
             }
-            // Reproduce Generation from CurrentGeneration
-            else
+            else // Reproduce Generation from CurrentGeneration 
             {
                 this.CurrentGeneration = ReproduceNextGeneration();
+                this.GenerationCount++;
                 return this.CurrentGeneration;
             }
-
         }
 
         /// <summary>
-        /// This method creates a new generation based Elite Chromosomes 
+        /// This method creates a new generation based of Elite Chromosomes 
         /// of the current generation.
         /// </summary>
-        /// <returns> A new generation of chromosome with IGeneration </returns>
+        /// <returns> A new reproduced generation of chromosomes </returns>
         private IGeneration ReproduceNextGeneration()
         {
             IChromosome[] newGenerationChromosome = new IChromosome[PopulationSize];
-            List<IChromosome> eliteChromsome = new List<IChromosome>();
-            // Define fitness of the elite chromosomes
-            
-            // double eliteChromosomeFitnessLowerLimit = this.CurrentGeneration.MaxFitness - 30;
-            // // Select the elite chromosomes
-            // for (int i = 0; i < this.PopulationSize; i++)
-            // {       
-            //     if (this.CurrentGeneration[i].Fitness >= eliteChromosomeFitnessLowerLimit)
-            //     {
-            //         // Check elite rate to add to list
-            //         if (this._random.NextDouble() <= (this.EliteRate / 100.00))
-            //         {
-            //             eliteChromsome.Add(this.CurrentGeneration[i]);
-            //         }
-            //     }
-            // }
+        
+            // Get All Elite Chromsomes
+            int eliteChromosomePopulationSize = (int)(this.PopulationSize * (this.EliteRate / 100.00));
+            IChromosome[] eliteChromsome = new Chromosome[this._eliteChromosomePopulationSize];
 
-            // Teacher Way???
-            // Get eliteChromosomes from elite rat
-            // int eliteChromosomePopulationSize = (int)(this.PopulationSize * (this.EliteRate / 100.00));
-            // Add a property internal of the chromsome array
-            IChromosome[] chrom = new IChromosome[12];
-            var allChromsomes = chrom.OrderByDescending(x => x.Fitness).Take(_eliteChromosomePopulationSize).ToArray();
+            eliteChromsome = this.CurrentGeneration.ChromosomeArr.OrderByDescending(x => x.Fitness).Take(_eliteChromosomePopulationSize).ToArray();
 
-            // Get highest fitness chromosomes
-            for (int i = 0; i < allChromsomes.Length; i++)
+            // Copy Elite Chromosomes to new Generation
+            for (int i = 0; i < eliteChromsome.Length; i++)
             {
-                eliteChromsome.Add(allChromsomes[i]);
-            }
-
-
-            // // Select the elite chromosomes
-            // for (int i = 0; i < this.PopulationSize; i++)
-            // {       
-            //     if (this.CurrentGeneration[i].Fitness >= eliteChromosomeFitnessLowerLimit)
-            //     {
-            //         // Check elite rate to add to list
-            //         if (this._random.NextDouble() <= (this.EliteRate / 100.00))
-            //         {
-            //             eliteChromsome.Add(this.CurrentGeneration[i]);
-            //         }
-            //     }
-            // }
-
-            // Add elite chromosomes to new generation
-            for (int j = 0; j < eliteChromsome.Count; j++)
-            {
-                newGenerationChromosome[j] = eliteChromsome[j];
+                newGenerationChromosome[i] = eliteChromsome[i];
             }
 
             // Create the rest of the chromosomes
-            // z is increased by 2 because we are adding 2 chromosomes at a time.
-            for (int z = eliteChromsome.Count; z < this.PopulationSize; z +=2)
+            // index is increased by 2 because we are adding 2 chromosomes at a time.
+            for (int z = eliteChromsome.Length; z < this.PopulationSize; z += 2)
             {
                 // Select the parents
                 IChromosome[] children = GetReproducedChildren(eliteChromsome);
 
                 newGenerationChromosome[z] = children[0];
                 // Check if there is space for another child
-                if (z+1 < this.PopulationSize)
+                if (z + 1 < this.PopulationSize)
                 {
-                    newGenerationChromosome[z+1] = children[1];
+                    newGenerationChromosome[z + 1] = children[1];
                 }
             }
 
-            return new Generation(newGenerationChromosome);
+            return new Generation(newGenerationChromosome, this, this.FitnessCalculation, this._seed);
         }
 
         /// <summary>
@@ -173,15 +136,16 @@ namespace GeneticAlgorithm
         /// and returns their reproduced children.
         /// </summary>
         /// <returns> Two children Chromosome created by elite Chromosomes. </returns>
-        private IChromosome[] GetReproducedChildren(List<IChromosome> eliteChromsome)
+        private IChromosome[] GetReproducedChildren(IChromosome[] eliteChromsome)
         {
-            IChromosome parentA = eliteChromsome[this._random.Next(eliteChromsome.Count)];
-            IChromosome parentB = eliteChromsome[this._random.Next(eliteChromsome.Count)];
+            IChromosome parentA = eliteChromsome[this._random.Next(eliteChromsome.Length)];
+            IChromosome parentB = eliteChromsome[this._random.Next(eliteChromsome.Length)];
+            // Checks that two Parents are not the same
             while (true)
             {
                 if (parentB.CompareTo(parentA) == 0)
                 {
-                    parentB = eliteChromsome[this._random.Next(eliteChromsome.Count)];
+                    parentB = eliteChromsome[this._random.Next(eliteChromsome.Length)];
                     break;
                 }
             }
