e43e660554d7c4771f1aaca3deab0d23910e703d
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-11-07 15:52:50 -0500

    create GenerationTests class skeleton

---
+++ GeneticAlgorithmTests/GenerationTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GeneticAlgorithmTests
+{
+    [TestClass]
+    public class GenerationTests
+    {
+        
+    }
+}
