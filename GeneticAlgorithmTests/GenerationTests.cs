3a3bbced56c92a7f37b07fba045e6fbe1657ec58
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-11-08 12:13:19 -0500

    add skeleton methods and comments

--- GeneticAlgorithmTests/GenerationTests.cs
+++ GeneticAlgorithmTests/GenerationTests.cs
@@ -6,6 +6,32 @@ namespace GeneticAlgorithmTests
     [TestClass]
     public class GenerationTests
     {
-        
+        // test calculateAverageFitness helper method
+        [TestMethod]
+        public void TestAverageCalculation()
+        {
+            Generation gen = new Generation()
+        }
+
+        // test calculateMaxFitness helper method
+        [TestMethod]
+        public void TestMaxCalculation()
+        {
+
+        }
+
+        // Test that SelectParent returns an IChromosome in the correct range
+        [TestMethod]
+        public void TestSelectParent()
+        {
+
+        }
+
+        // Tests the EvaluatePopulationFitness method
+        [TestMethod]
+        public void TestEvaluatePopFitness()
+        {
+            
+        }
     }
 }
