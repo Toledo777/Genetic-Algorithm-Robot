2824a2bc50c164126a253821721762562ecca2f8
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-11-13 13:36:11 -0500

    complete TestAverageCalculation

--- GeneticAlgorithmTests/GenerationTests.cs
+++ GeneticAlgorithmTests/GenerationTests.cs
@@ -1,7 +1,4 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll;
-using Microsoft.VisualStudio.TestPlatform.TestFramework.Extensions.dll;
-using System;
 using GeneticAlgorithm;
 
 namespace GeneticAlgorithmTests
@@ -23,23 +20,30 @@ namespace GeneticAlgorithmTests
 
         // test the average calculation 
         [TestMethod]
-        [DataRowAttribute(200, 243, 7, 40, 30, 20)]
-        public void TestAverageCalculation(int populationSize, int numberOfGenes, int lengthOfGene, double mutationRate, double eliteRate, int numberOfTrials)
+        [DataRowAttribute(200, 243, 7, 40, 30, 20, 2235)]
+        [DataRowAttribute(200, 243, 7, 40, 30, 20, 98497)]
+        [DataRowAttribute(200, 243, 7, 40, 30, 20, 948545)]
+        [DataRowAttribute(200, 243, 7, 40, 30, 20, 53894)]
+        public void TestAverageCalculation(int populationSize, int numberOfGenes, int lengthOfGene, double mutationRate, double eliteRate, int numberOfTrials, int seed)
         {
             GeneticAlgorithm.GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm.GeneticAlgorithm(populationSize, numberOfGenes, lengthOfGene, mutationRate, eliteRate, numberOfTrials, handler, seed);
             IGeneration gen = geneticAlgorithm.GenerateGeneration();
-            double average = gen.AverageFitness;
+            double actualAverage = gen.AverageFitness;
+            double expected = 0;
 
             for (int i = 0;  i < gen.NumberOfChromosomes; i++) {
-                
+                expected += gen[i].Fitness;
             }
-            
 
+            // computer average
+            expected = expected / gen.NumberOfChromosomes;
+
+            Assert.AreEqual(expected, actualAverage);
         }
 
         // test max fitness calculation
         [TestMethod]
-        public void TestMaxCalculation(int populationSize, int numberOfGenes, int lengthOfGene, double mutationRate, double eliteRate, int numberOfTrials)
+        public void TestMaxCalculation(int populationSize, int numberOfGenes, int lengthOfGene, double mutationRate, double eliteRate, int numberOfTrials, int seed)
         {
             GeneticAlgorithm.GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm.GeneticAlgorithm(populationSize, numberOfGenes, lengthOfGene, mutationRate, eliteRate, numberOfTrials, handler, seed);
             IGeneration gen = geneticAlgorithm.GenerateGeneration();
@@ -47,7 +51,7 @@ namespace GeneticAlgorithmTests
 
         // Test that SelectParent returns an IChromosome in the correct range
         [TestMethod]
-        public void TestSelectParent(int populationSize, int numberOfGenes, int lengthOfGene, double mutationRate, double eliteRate, int numberOfTrials)
+        public void TestSelectParent(int populationSize, int numberOfGenes, int lengthOfGene, double mutationRate, double eliteRate, int numberOfTrials, int seed)
         {
             GeneticAlgorithm.GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm.GeneticAlgorithm(populationSize, numberOfGenes, lengthOfGene, mutationRate, eliteRate, numberOfTrials, handler, seed);
             IGeneration gen = geneticAlgorithm.GenerateGeneration();
@@ -55,7 +59,7 @@ namespace GeneticAlgorithmTests
 
         // Tests the EvaluatePopulationFitness method
         [TestMethod]
-        public void TestEvaluatePopFitness(int populationSize, int numberOfGenes, int lengthOfGene, double mutationRate, double eliteRate, int numberOfTrials)
+        public void TestEvaluatePopFitness(int populationSize, int numberOfGenes, int lengthOfGene, double mutationRate, double eliteRate, int numberOfTrials, int seed)
         {
             GeneticAlgorithm.GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm.GeneticAlgorithm(populationSize, numberOfGenes, lengthOfGene, mutationRate, eliteRate, numberOfTrials, handler, seed);
             IGeneration gen = geneticAlgorithm.GenerateGeneration();
