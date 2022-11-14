24967c66e5371e528891632e60643ef065fa20cb
Maxime_Rayvich <maximerayvich@gmail.com>
2022-11-14 12:03:11 -0500

    add reference

---
+++ RobbyVisualizer/tile.cs
@@ -0,0 +1,11 @@
+namespace RobbyVisualizer
+{
+    internal class Tile{
+        private bool _isCan;
+        private bool _isRobby;
+        public Tile(bool isCan, bool isRobby){
+            this._isCan = isCan;
+            this._isRobby = isRobby;
+        }
+}
+}
